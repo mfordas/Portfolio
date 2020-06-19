d393ac531a19c0a358762a8425c29b2e26965648
Mateusz <mateusz.fordas@gmail.com>
2020-06-19 16:16:13 +0200

    Modified modal

--- src/views/AppContent/AppContent.jsx
+++ src/views/AppContent/AppContent.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { TiSocialLinkedinCircular} from 'react-icons/ti';
 import Modal from 'react-modal';
 import { NavLink } from 'react-router-dom';
 import { TiHome } from 'react-icons/ti';
@@ -6,6 +7,7 @@ import { TiHome } from 'react-icons/ti';
 import Menu from '../../components/Menu';
 import Projects from '../../components/Projects';
 import Footer from '../../components/Footer';
+import photoSrc from '../../img/photo.png';
 import '../../styling/main_styling.scss'
 
 Modal.setAppElement('#root');
@@ -46,6 +48,8 @@ const AppContent = () => {
                     <h2>Still here?</h2>
                     <div>You are here already 30s! Did you know that average visit time on website is 15s?</div>
                     <div>This means you like it! :) Write me a message or take a phone and make a call. There's no reason to wait any longer.</div>
+                    <img className='photo' src={photoSrc} alt='myphoto'/>
+                    <a href='https://www.linkedin.com/in/mateusz-fordas/' rel="noopener noreferrer" target='_blank'><TiSocialLinkedinCircular/></a>
                     <button className="modalButton" onClick={closeModal}>Close</button>
                 </Modal>
                 <NavLink className='mainPageLink' to="/Portfolio/projects/main"><TiHome /></NavLink>
