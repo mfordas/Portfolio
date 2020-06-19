a729585d343e72a107cd7085ee3c183330d65f22
Mateusz <mateusz.fordas@gmail.com>
2020-06-19 17:27:38 +0200

    Changed function for scrolling window

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -6,11 +6,18 @@ import '../../styling/main_styling.scss';
 
 const FooterContent = () => {
 
-    
+    const moveToTop = () => {
+        window.scroll({
+            top: 0, 
+            left: 0, 
+            behavior: 'smooth' 
+           });
+    }
 
     return (
         <footer className="containerFooterHorizontal">
         <p>Copyright © <a target="_blank" rel="noopener noreferrer" href="https://fordas.pl" >Mateusz Fordas</a> 2020 </p>
+        <p onClick={() => moveToTop()}><TiArrowUpThick/></p>
         </footer>
     );
 }
