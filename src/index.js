e2021fce0fe9e132e23f508b7456eeba47d0d4d5
Mateusz <mateusz.fordas@gmail.com>
2020-08-08 15:50:03 +0200

    Added google analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/App/App';
 
+const loadScript = () => {
+  const script = document.createElement('script');
+  script.onload = function () {
+      window.dataLayer = window.dataLayer || [];
+    
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', 'UA-149871373-1');
+    };
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-149871373-1';
+  document.head.appendChild(script);
+};
+
+loadScript();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
